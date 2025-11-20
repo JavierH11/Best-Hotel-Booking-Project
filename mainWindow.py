c1bb8f726556020cba9e0b1cd88303ab6e7ab54e
Matthew Cabrera <matthew.cabrera.988@my.csun.edu>
2025-11-19 21:48:09 -0800

    Main Window for app

---
+++ mainWindow.py
@@ -0,0 +1,20 @@
+"""
+Hotel Booking System - Main Entry Point
+
+This module serves as the entry point for the application. 
+Run this file to start the Hotel Booking System.
+
+Usage:
+    python main.py
+
+The main.py file initializes the tkinter window and starts the application event loop
+"""
+
+import tkinter as tk
+#from app import HotelBookingApp
+
+def main():
+    print("THIS WORKS")
+
+if __name__ == "__main__":
+    main()
