6c9f4fb25a54473c591ca4a22b910041b065517b
Matthew Cabrera <matthew.cabrera.988@my.csun.edu>
2025-11-20 15:10:43 -0800

    updated app file

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ screens and user interactions
 Classes:
     HotelBookingApp: Main application controller
 """
-
 import tkinter as tk
 from tkinter import ttk, messagebox, scrolledtext
 from datetime import datetime, timedelta
@@ -15,10 +14,10 @@ from datetime import datetime, timedelta
 # Import from other modules
 from models import Room
 from utils import validate_date, generate_conf_number
-#from storage import load_bookings, find_booking
+from storage import load_bookings, find_booking
 from createReservation_logic import create_reservation, modify_reservation, cancel_reservation
 from room_logic import get_available_rooms
-#from email_service import send_email
+from email_service import send_email
 
 class HotelBookingApp:
     """
